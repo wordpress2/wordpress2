14d33f748be50a7762afe3f77ab2c330c106e8f2
master <git.wordpress.2@gmail.com>
2018-05-10 13:03:33 +0900

    New: asset/config.php - Time::Timezone()

--- asset/config.php
+++ asset/config.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * wordpress2:/config.php
+ *
+ * @creation  2018-03-27
+ * @version   1.0
+ * @package   wordpress2
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+//	Time
+Time::Timezone('Asia/Tokyo');
+
 //	Layout settigs.
 Layout::Directory(__DIR__.'/layout');
 Layout::Execute(true);
